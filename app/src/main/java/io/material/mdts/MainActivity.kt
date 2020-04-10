a7949a3c7b8e4f6e9583ff5508fab74ecfe1f07c
Nick Rout <nickrout@google.com>
2020-04-10 16:37:12 +0200

    Remove FAB

--- app/src/main/java/io/material/mdts/MainActivity.kt
+++ app/src/main/java/io/material/mdts/MainActivity.kt
@@ -3,7 +3,6 @@ package io.material.mdts
 import android.os.Bundle
 import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.TooltipCompat
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
@@ -19,7 +18,6 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setSupportActionBar(toolbar)
-        TooltipCompat.setTooltipText(fab, getString(R.string.add))
         appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.home,
